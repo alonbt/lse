51ecd4a0a8a5042cb9667d4d849c7986a2049d86
unknown <alonbt@wix.com>
2012-04-03 17:32:43 +0300

    js changes

--- js/myScript.js
+++ js/myScript.js
@@ -79,8 +79,10 @@ var ls = {
 	},
 	createNew : function() {
 			$('#ls_data').prepend('<div class="ls_row_wrapper" id="ls_row_wrapper_sample"><div class="header"><div class="indicationArrow">&nbsp;</div><span id="ls_name" class="ls_name">New Field</span> <a href="#" class="editName"><span class="img"></span>Save Value</a></div><div class="content"><input type="text" value="Enter Value" class="ls_value"><div class="content_links"><a class="delete" href="#"><span class="img"></span>Delete</a></div></div></div>')
-			$('#ls_data .ls_row_wrapper:first-child').find('.header').css({'height' : 0, 'overflow':'hidden'}).animate({'height':'21px'},500,function(){
-				$(this).css({'height':'','overflow' : ''});
+			$('#ls_data .ls_row_wrapper:first-child').css({'height' : 0, 'overflow':'hidden', 'opacity' : '0'}).animate({'height':'34px'},250,function(){
+				$(this).animate({'opacity' : '1'},250, function() {
+					$(this).css({'height':'','overflow' : ''});
+				});
 			});
 			ls.initSingleRow($('#ls_data .ls_row_wrapper:first-child'));
 			$('#ls_data .ls_row_wrapper:first-child').find('.editName').click();
